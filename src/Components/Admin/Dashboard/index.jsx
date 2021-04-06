f7780341dd5e8625d4a48aa140f1f5b1300c9c2f
Andres <vmd211099@gmail.com>
2021-04-06 17:38:14 +0700

    edit

--- src/Components/Admin/Dashboard/index.jsx
+++ src/Components/Admin/Dashboard/index.jsx
@@ -5,20 +5,28 @@ function Dashboard() {
         <div className="dashboard">
             <div className="flex">
                 <div className="box-color display-total">
-                    <h1>0</h1>
+                    <h1 className="title_w">0</h1>
                     <p>Total order</p>
                 </div>
                 <div className="box-color display-total">
-                    <h1>0</h1>
+                    <h1 className="title_w">0</h1>
                     <p>Total order</p>
                 </div>
                 <div className="box-color display-total">
-                    <h1>0</h1>
+                    <h1 className="title_w">0</h1>
                     <p>Total order</p>
                 </div>
                 <div className="box-color display-total">
-                    <h1>0</h1>
+                    <h1 className="title_w">0</h1>
                     <p>Total order</p>
+                </div>
+
+            </div>
+            <div className="flex">
+                <div className="box-color transaction-history">
+                    <h1 className="title_w">Transaction history</h1>
+                </div>
+                <div className="box-color">
                 </div>
 
             </div>
